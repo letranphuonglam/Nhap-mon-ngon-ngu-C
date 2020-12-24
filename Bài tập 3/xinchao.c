ecbccb97df35ea60fe22bd2832664ee234b7951f
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2020-12-24 08:46:31 +0700

    Add files via upload

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	// khao bao
+    int count,
+	// Hen thi 10 dong xin chao
+   for(count = 1;count <=10;count++)
+      printf("\n \t \t xin chao");
+    return 0;
+}
