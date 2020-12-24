b1804d6bdedf71fd45135f90c0f7a68c607a6cf0
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2020-12-24 11:04:51 +0700

    Update tong10.c

--- Bài tập 3/tong10.c
+++ Bài tập 3/tong10.c
@@ -4,20 +4,13 @@ int main(void) {
   //khai bao bien tong tu 1 dem 10
 
   int tong;
-  tong = tong +1;
-  tong = tong + 2;
-  tong = tong + 3;
-  tong = tong + 4;
-  tong = tong + 5;
-  tong = tong + 6;
-  tong = tong + 7;
-  tong = tong + 8;
-  tong = tong + 9;
-  tong = tong + 10;
+  // tong = 1 + 2 + 3 +....+10
+  tong = 0;
   //vong lap for
   int dem;
-  for(dem = 1;dem <=10;dem++)
+  for(dem = 1;dem <=10;dem++){
     tong = tong + dem;
+  }
       printf("Tong la: %d",tong);
   return 0;
 }
