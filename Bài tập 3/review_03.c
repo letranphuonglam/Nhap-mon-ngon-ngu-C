3fad418e01e5742f70907358537916da2294c23a
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2020-12-31 10:58:52 +0700

    Add files via upload

---
+++ Bài tập 3/review_03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+	//Khai bien tu 1=>20
+  int a;
+  for(a = 1; a < 21; a= a + 2){
+	if(a != 5){
+    }
+	   if(a != 11){
+	}   	
+        if(a != 15){
+    }    	
+      printf("%d \n",a);
+    
+  }
+return 0;
+}
