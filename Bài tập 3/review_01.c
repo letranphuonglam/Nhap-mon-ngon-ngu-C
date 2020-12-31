f416157f355842c8c36ec778f485f3f66305b092
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2020-12-31 08:46:31 +0700

    Add files via upload

---
+++ Bài tập 3/review_01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+	//Khai bao bien 
+	int n;
+	
+	//Nhap du lieu n
+	printf("Nhap du lieu n = ");
+	scanf("%d", &n);
+	
+		if (n==1);{
+			printf("one");
+		
+		}else if(n == 2){
+			printf("two");
+		}else if (n == 3){
+			printf("three");
+		}else if (n == 4){
+			printf("four");
+		}else if (n == 5){
+			printf("five");
+		}else if (n == 6){
+			printf("six");
+		}else if (n == 7){
+			printf("seven");
+		}else if (n == 8){
+			printf("eghit");
+		}else if (n == 9){
+			 printf("nice",n);
+ 	    }else if(n >9){
+			printf("Greater than 9");
+		 }
+  return 0;
+}	
