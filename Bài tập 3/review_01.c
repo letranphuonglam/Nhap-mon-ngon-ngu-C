77ccce8e62b359d46ef01c7fb108b7fd5ed5fc27
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2020-12-31 09:06:40 +0700

    Update review_01.c

--- Bài tập 3/review_01.c
+++ Bài tập 3/review_01.c
@@ -10,24 +10,33 @@ int main(void) {
 	
 		if (n==1);{
 			printf("one");
+		}
 		
-		}else if(n == 2){
+		else if(n == 2){
 			printf("two");
-		}else if (n == 3){
+		}
+		else if (n == 3){
 			printf("three");
-		}else if (n == 4){
+		}
+		else if (n == 4){
 			printf("four");
-		}else if (n == 5){
+		}
+		else if (n == 5){
 			printf("five");
-		}else if (n == 6){
+		}
+		else if (n == 6){
 			printf("six");
-		}else if (n == 7){
+		}
+		else if (n == 7){
 			printf("seven");
-		}else if (n == 8){
+		}
+		else if (n == 8){
 			printf("eghit");
-		}else if (n == 9){
+		}
+		else if (n == 9){
 			 printf("nice",n);
- 	    }else if(n >9){
+		}
+ 	   	else if(n >9){
 			printf("Greater than 9");
 		 }
   return 0;
