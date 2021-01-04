f2d3350e168a2916c056b2d05528e5c2b2c89cd9
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-04 18:09:05 +0700

    Add files via upload

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 int main(){
-	//khai bao bien
-	int i,j;
-	printf("\t------CUU CHUONG\n-----")
-	//xuat bang cuu chuong
- 	for(i = 1; i < 11; i++);
-	{
-	for (j = 1; j < 10; j++);
-	{
-		printf("%d*%d=%d\t", j, i, j * i);
+	int a,b;
+  	//khai bao bien
+	printf("Bang cuu chuong\n");
+  	//xuat ra nan hinh
+	for (a=1;a<=9;a++){
+		printf("Bang cuu chuong %d\n",a);
+		for (b=1;b<=9;b++){
+			printf("%d * %d = %d\n",a,b,a*b);
+		}
 	}
-		printf("\n");
-	}
-	return 0;
-	
-	}
+}
