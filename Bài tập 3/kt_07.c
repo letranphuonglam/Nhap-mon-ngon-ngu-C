3d5f5afc0659b8317af6d97b461080b734517d93
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-04 17:43:56 +0700

    Add files via upload

---
+++ Bài tập 3/kt_07.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a, b, c;
+	printf ("\n Nhap ba so a,b,c:");
+	scanf ("%d%d%d", &a, &b, &c);
+	if (a>0 && b>0 && c>0){
+		if (abs (a-c)<b && b<(a+c));
+		printf ("\nBa tao thanhc mot tam giac !");
+	}else{
+		printf ("\nBa khong tao thnah mot tam giac !");
+	}
+ 	}
