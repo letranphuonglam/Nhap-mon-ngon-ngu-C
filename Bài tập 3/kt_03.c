ca8bc0af4ab87cf1c0f51ef13fa735cca48d1803
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-06 10:13:38 +0700

    Add files via upload

---
+++ Bài tập 3/kt_03.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+int main(){
+	int n;
+	for (n=1; n<=20; n++){
+		if (n%2!=0){
+		switch (n){
+			case 5:
+				break;
+			case 11:
+				break;
+			case 15:
+				break;
+			default:
+				printf ("\n %d ",n); 
+			}
+
+			}
+			
+		}
+	}
+	
