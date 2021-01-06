3b98fa7317dcf68f66b1af5b0ce8f261196a0442
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-06 10:21:37 +0700

    Update kt_07.c

--- Bài tập 3/kt_07.c
+++ Bài tập 3/kt_07.c
@@ -5,8 +5,8 @@ int main(){
 	scanf ("%d%d%d", &a, &b, &c);
 	if (a>0 && b>0 && c>0){
 		if (abs (a-c)<b && b<(a+c));
-		printf ("\nBa tao thanh mot tam giac !");
+		printf ("\nBa tao canh tao thanh mot tam giac !");
 	}else{
-		printf ("\nBa khong tao thnah mot tam giac !");
+		printf ("\nBa khong khong tao thanh mot tam giac !");
 	}
  	}
