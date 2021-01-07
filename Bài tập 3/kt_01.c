ad4b486da0a4673fd4f2041ff90a51dbc3c337d6
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-07 11:18:26 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -1,13 +1,24 @@
-#include <stdio.h>
-int main(){
-	int a,b;
-  	//khai bao bien
-	printf("Bang cuu chuong\n");
-  	//xuat ra nan hinh
-	for (a=1;a<=10;a++){
-		printf("Bang cuu chuong %d\n", a);
-	for(b=1;b<=10;b++){
-		printf("%d * %d = %d\n", a, b, a*b);
-		}
-	}
+#include<stdio.h>
+int main (){
+  int a, b, c;
+  a = 2, b = 3, c = 4;
+  //hien thi ra man hinh BAN CUU CHUONG
+  printf("\t                         BANG CUU CHUONG\n");
+  //hien thi BAN CUU CHUONG 2 3 4 , 3 dong dau
+  printf("BANG CUU CHUONG 2\n");
+  printf("%d x 1 = %d\n", a, a * 1);
+  printf("%d x 2 = %d\n", a, a * 2);
+  printf("%d x 3 = %d\n", a, a * 3);
+  
+  printf("BANG CUU CHUONG 3\n");
+  printf("%d x 1 = %d\n", b, b * 1);
+  printf("%d x 2 = %d\n", b, b * 2);
+  printf("%d x 3 = %d\n", b, b * 3);
+  
+  printf("BANG CUU CHUONG 4\n");
+  printf("%d x 1 = %d\n", c, c * 1);
+  printf("%d x 2 = %d\n", c, c * 2);
+  printf("%d x 3 = %d\n", c, c * 3);
+  
+    return 0; 
 }
