f0c5eac164dcfebe089f9886342c22c586133049
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-07 09:53:11 +0700

    Add files via upload

---
+++ Bài tập 3/kt_04.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int n,sum=0;
+	//Xuat ra tong cac chu so 
+    printf("Nhap so n: ");
+	scanf("%d",&n);
+   
+    for(;n!=0;n=n/10){
+    }    
+    printf("%d",n);
+    return 0;
+    }
