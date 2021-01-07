e0fb2e2b120eb456918403bb493f9372877dd4f5
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-07 09:59:43 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -6,8 +6,8 @@ int main(){
   	//xuat ra nan hinh
 	for (a=1;a<=10;a++){
 		printf("Bang cuu chuong %d\n", a);
-		for(b=1;b<=10;b++){
-			printf("%d * %d = %d\n", a, b, a*b);
+	for(b=1;b<=10;b++){
+		printf("%d * %d = %d\n", a, b, a*b);
 		}
 	}
 }
