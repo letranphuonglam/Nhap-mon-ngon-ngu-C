aca019442870c1691dcba91e9cf795dd62050530
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-14 10:23:07 +0700

    Add files via upload

---
+++ Bài tập 4/mang.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int i=0;
+  //khai bao
+  int mark[4] = {3,5,7,2};
+  for(i=0;i<4;i++){
+  //hien thi mang ra man hinh
+  printf("%d\n",mark[i]);
+  //khai bao tong
+  }
+  int tong;
+  tong= 3+5+7+2;
+  printf("%d",tong);
+  
+  return 0;
+}
