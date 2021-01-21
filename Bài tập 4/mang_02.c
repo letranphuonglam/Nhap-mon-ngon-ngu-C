2f0779feef9b3a7b4820ee8c4d029f724c75f24f
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-21 08:20:11 +0700

    Add files via upload

---
+++ Bài tập 4/mang_02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main () {
+  //Khai bao bien kem mang a kich thuoc bang 100 (0<n<=100)
+  int a[100];
+  int n, i;
+  //Khai bao bien c de tinh tong
+  int tong = 0;
+      //xuat dong chu nhap kich thuoc mang
+  		printf("Nhap kich thuoc mang: \n");
+  		scanf("%d", &n);
+      //xuat dong chu Nhap so phan tu ra man hinh
+      	printf("\nNhap so phan tu:");
+ 	
+	 	for	(i = 0; i < n; i++){
+      	scanf("%d", &a [i]);
+		}
+ 	 //Vong lap Tong cac so tu day tren
+		for ( i = 0; i < n; i++){
+		tong = tong + a[i];
+		}
+    //xuat ket qua tinh tong ra man hinh 
+		printf("\nTong cac so tu day tren = %d", tong);
+  
+  return  0;
+}
