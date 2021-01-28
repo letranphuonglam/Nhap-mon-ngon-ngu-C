496a8e75a2a313cceba5e8e2be7b7a5e3bcccf64
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-28 14:10:14 +0700

    Update ham_01.c

--- Bài tập 5/ham_01.c
+++ Bài tập 5/ham_01.c
@@ -1,59 +1,59 @@
-//Khoi khai bao
-#include<stdio.h>
-void Nhap_pt_mang(int a[]);
-void Hien_thi_pt_mang(int a[]);
-void Hien_thi_tong_chan(int a[]);
-void Dem_chan_le_0(int a[]);
-int n;
-//khoi ham main
-int main(){
+//khoi khai bao
+#include <stdio.h>
 
-  printf("Nhap n = ");
-  scanf("%d", &n);
-  int a[n];
-  Nhap_pt_mang(a);
-  Hien_thi_pt_mang(a);
-  Hien_thi_tong_chan(a);
-  Dem_chan_le_0(a);
+// khai bao ham sum
+int sum(int a,int b,int c);
+// khoi ham main 
+int main() {
+int a,b,c;// goi ham sum
+printf("Nhap 3 so nguyen tu ban phim:\n");
+	printf("a = ");
+	scanf("%d",&a);// nhap a
+
+	printf("b = ");
+	scanf("%d",&b);// nhap b
+
+	printf("c = ");
+	scanf("%d",&c);// nhap c
+
+printf("a + b + c = %d",sum(a,b,c));
+
   return 0;
 }
-
 // khoi dinh nghia ham
-void Nhap_pt_mang(int a[]){
-	int i;
-	for (i=0;i<n;i++){
-		printf("Nhap a[%d]= ", i);scanf("%d",&a[i]);
-	}
+int sum(int a,int b,int c){
+  return a + b + c;
 }
 
-void Hien_thi_pt_mang(int a[]){
-	int j;
-	printf("Mang a:");
-	for (j=0;j<n;j++){
-		printf(" %d ",a[j]);
-	}
-	printf("\n");
-}
 
-void Hien_thi_tong_chan(int a[]){
-	int i,even_sum=0;
-	for (i=0;i<n;i++){
-		if (a[i]%2==0){
-			even_sum+=a[i];
-		}
-	}
-	printf("Tong cac so chan trong mang = %d\n",even_sum);
-}
 
-void Dem_chan_le_0(int a[]){
-	int j,j_even=0,j_odd=0;
-	for (j=0;j<n;j++){
-		if (a[j]%2==0){
-			j_even+=1;
-		}
-		else{
-			j_odd+=1;
-		}
-	}
-	printf("Mang co %d so chan, %d so le",j_even,j_odd);
+// Khong co kieu du lieu tra ve (void)
+
+// khoi khai bao
+#include <stdio.h>
+
+// khai bao ham sum
+void sum(int a,int b,int c);
+// khoi ham main 
+int main() {
+int x,y,z;// goi ham sum
+printf("Nhap 3 so nguyen tu ban phim:\n");
+	printf("x = ");
+	scanf("%d",&x);// nhap x
+
+	printf("y = ");
+	scanf("%d",&y);// nhap y
+
+	printf("z = ");
+	scanf("%d",&z);// nhap z
+
+	sum(x,y,z);
+
+  return 0;
+}
+// khoi dinh nghia ham
+void sum(int a,int b,int c){
+int d = a + b + c;
+printf("x + y + z = %d",d);
+   
 }
