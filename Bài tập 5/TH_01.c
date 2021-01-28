178acd25da789f1abea0ca14c00258c8be9f7c3c
letranphuonglam <72535110+letranphuonglam@users.noreply.github.com>
2021-01-28 15:47:27 +0700

    Update TH_01.c

--- Bài tập 5/TH_01.c
+++ Bài tập 5/TH_01.c
@@ -1,45 +1,30 @@
-#include <stdio.h>
-#include <math.h>
-void Enter_number();
-void result(int a,int b,int c);
-int a,b,c;
-float delta,x,x1,x2;
-
-void main(){
-	//Nhap phuong trinh
-	Enter_number();
-	
-	//Tinh nghiem cua phuong trinh
-	result(a,b,c);
-}
-
-void Enter_number(){
-	//Nhap phuong trinh
-	printf("Phuong trinh co dang ax^2 + bx + c = 0\n");
-	printf("Nhap hang so phuong trinh\n");
-	printf("a = ");scanf("%d",&a);
-	printf("b = ");scanf("%d",&b);
-	printf("c = ");scanf("%d",&c);
-}
-
-void result(int a,int b,int c){
-	//ham pow(a,b) tra ve gia tri la a^b 
-	delta=pow(b,2)-4*a*c;
-	
-	//Tinh nghiem cua phuong trinh
-	if (delta >0){
-		x1=(-b + sqrt(delta))/(2*a);
-		x2=(-b - sqrt(delta))/(2*a);
-		printf("Phuong trinh %dx^2 + %dx + %d = 0 co 2 nghiem\n",a,b,c);
-		printf("x1 = %.2f\n",x1);
-		printf("x2 = %.2f\n",x2);
-	}
-	else if (delta == 0){
-		x= -b/(2*a);
-		printf("Phuong trinh %dx^2 + %dx + %d = 0 có nghiem kep\n",a,b,c);
-		printf("x = %.f\n",x);
-	}
-	else{
-		printf("Phuong trinh %dx^2 + %dx + %d = 0 vo nghiem\n",a,b,c);
-	}
+#include "stdio.h"
+#include "math.h"
+int main(){
+ 	// Khai bao
+ 	 float a,b,c,x1,x2, delta;
+	 // Nhap lieu
+ 	printf("Nhap vao he so a, b, c");
+ 	scanf("%f%f%f", &a, &b, &c);
+ 
+ 	// Tinh toan va xuat ket qua
+	 if (a==0){
+	 printf("\nHe so a phai khac 0");
+	 }else{
+ 	 // a !=0
+ 	 delta = pow(b, 2)-4*a*c;
+ 	 printf("\ndelta = %.2f", delta);
+  
+ 	 if (delta > 0){
+ 	  x1 = (-b-sqrt(delta))/(2*a);
+ 	  x2 = (-b+sqrt(delta))/(2*a);
+  	 	printf("\nPhuong trinh co nghiem phan biet: x1=%.2f va x2=%.2f", x1, x2);
+ 	 }else if(delta==0){
+ 	  x1 = -b/(2*a);
+  		 printf("\nPhuong trinh co nghiem kep x1=x2=%.2f", x1);
+ 	 }else{
+ 		printf("\nPhuong trinh vo nghiem");
+  }
+  
+ }
 }
